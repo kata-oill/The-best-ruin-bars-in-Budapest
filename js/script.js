d2a72dc20619c002587d8c1f2af9d7051cb5eac8
Katarzyna Grabowska <kati.grab@gmail.com>
2020-01-15 00:20:23 +0100

    Add reset button function and scroll bar function

--- js/script.js
+++ js/script.js
@@ -2,4 +2,28 @@
 var flkty = new Flickity( '.carousel', {
   hash: true,
   pageDots: false,
+  groupCells: true,
+});
+
+
+var buttonGroup = document.querySelector('.button-group');
+var buttons = buttonGroup.querySelectorAll('.button');
+buttons = fizzyUIUtils.makeArray( buttons );
+
+buttonGroup.addEventListener( 'click', function( event ) {
+  // filter for button clicks
+  if ( !matchesSelector( event.target, '.button' ) ) {
+    return;
+  }
+  var index = buttons.indexOf( event.target );
+  flkty.selectCell( index );
+});
+
+var flkty = new Flickity('.carousel');
+
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
 });
