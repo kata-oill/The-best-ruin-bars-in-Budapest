ef1ec0f446b07d16f108249838409a39a7dc5c03
Katarzyna Grabowska <kati.grab@gmail.com>
2020-01-19 18:40:01 +0100

    Modify functions to genetrate content for slides

--- js/script.js
+++ js/script.js
@@ -1,24 +1,36 @@
 'use strict';
+// Google map
+
+var infos = document.getElementById('map-info');
+
+window.initMap = function() {
+  // The locations
+ 	var uluru = {lat: -25.344, lng: 131.036};
+ 	var coords2 = {lat: -25.363, lng: 134.044};
+ 	var coords3 = {lat: -25.363, lng: 137.044};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
 
 // Mustache template
 
 var templateSlide = document.getElementById('template-slide-carousel').innerHTML;
-var results = document.querySelectorAll('.content-container');
 
 Mustache.parse(templateSlide);
 
-var listSlides = '';
+var generateSlide = '';
 
 for(var i = 0; i < barList.length; i++){
 		console.log(barList);
-		listSlides += Mustache.render(templateSlide, barList[i]);
+		generateSlide += Mustache.render(templateSlide, barList[i]);
 	}
 
-var fullSlideList = Mustache.render(templateSlide, {slide: listSlides});
+var results = document.querySelector('.content-container');
 
-results.insertAdjacentHTML('beforeend', fullSlideList);
-
-
+results.insertAdjacentHTML('beforeend', generateSlide);
 
 // Flickity carousel
 
