a4ccc2afa127a9a808729d77ecec0aae8394954a
Katarzyna Grabowska <kati.grab@gmail.com>
2020-01-19 17:23:45 +0100

    Add Mustache template variables and functions

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
+'use strict';
+
+// Mustache template
+
+var templateSlide = document.getElementById('template-slide-carousel').innerHTML;
+var results = document.querySelectorAll('.content-container');
+
+Mustache.parse(templateSlide);
+
+var listSlides = '';
+
+for(var i = 0; i < barList.length; i++){
+		console.log(barList);
+		listSlides += Mustache.render(templateSlide, barList[i]);
+	}
+
+var fullSlideList = Mustache.render(templateSlide, {slide: listSlides});
+
+results.insertAdjacentHTML('beforeend', fullSlideList);
+
+
+
+// Flickity carousel
 
 var flkty = new Flickity( '.carousel', {
   hash: true,
